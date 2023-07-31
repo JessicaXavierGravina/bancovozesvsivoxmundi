4e2ea2145d3a38bd78f529cef1f3b16c9a0947e7
jessicaxavier <jessicabackend@gmail.com>
2023-07-31 10:41:59 -0300

    create superuser

--- vox/apps.py
+++ vox/apps.py
@@ -4,3 +4,14 @@ from django.apps import AppConfig
 class VoxConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'vox'
+
+    def ready(self):
+        from .models import Usuario
+        import os
+        email = os.getenv("EMAIL_ADMIN")
+        senha = os.getenv("SENHA_ADMIN")
+
+        usuarios = Usuario.objects.filter(email=email)
+        if not usuarios:
+            Usuario.objects.create_superuser(username="admin", email=email, password=senha, is_active=True, is_superuser=True)
+
