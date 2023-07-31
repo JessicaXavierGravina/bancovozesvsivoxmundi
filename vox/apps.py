e3b1195299aa532daee04dcfad872a6acb54881d
jessicaxavier <jessicabackend@gmail.com>
2023-07-31 11:27:23 -0300

    admin2

--- vox/apps.py
+++ vox/apps.py
@@ -10,8 +10,10 @@ class VoxConfig(AppConfig):
         import os
         email = os.getenv("EMAIL_ADMIN")
         senha = os.getenv("SENHA_ADMIN")
+        email2 = os.getenv("EMAIL_ADMIN2")
+        senha2 = os.getenv("SENHA_ADMIN2")
 
         usuarios = Usuario.objects.filter(email=email)
         if not usuarios:
-            Usuario.objects.create_superuser(username="admin", email=email, password=senha, is_active=True, is_superuser=True)
+            Usuario.objects.create_superuser(username="admin", email=email, password=senha, username2="admin2", email2=email2, password2=senha2, is_active=True, is_superuser=True)
 
